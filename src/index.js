abc40c54f9b58098711c46e2dd4fbc28d39517a0
Tijani Femi <olutimedia@gmail.com>
2021-07-02 23:08:57 +0100

    set up environment and mongoose db

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import '@babel/polyfill';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import logger from 'morgan';
+import router from './routes';
+import dbConnection from './config/dbconnection';
+
+const { NODE_ENV } = process.env;
+const app = express();
+
+// Public Folder
+app.use(express.static('./public'));
+
+dotenv.config();
+if (NODE_ENV === 'development' || NODE_ENV === 'production') {
+  app.use(logger('dev'));
+}
+app.use(express.text());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(cors());
+
+dbConnection();
+
+app.get('/api', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'Welcome to SMPA API',
+  });
+});
+
+app.use('/api/v1', router);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App is listening to port: ${port}`));
+
+export default app;
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+// import Auth from '../controllers/Auth';
+// import validator from '../middlewares/validator';
+
+const authRoute = Router();
+
+// authRoute.post('/signup_user', validator(userSchema), Auth.create);
+
+export default authRoute;
